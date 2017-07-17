9ae0c95b2d8e7786964c9c591fb27a89719eca7c
Frew A Negash <zulufanke@yahoo.com>
2017-07-17 13:34:34 -0400

    add project

---
+++ dictionary.js
@@ -0,0 +1,63 @@
+var list = [
+
+'concatenation',
+'string',
+'pull',
+'nab bar',
+'element',
+
+'github',
+'forking',
+'cloning',
+'command line',
+'scrum',
+
+'agile',
+'pop',
+'web development',
+'array',
+'object',
+
+'method',
+'class',
+'lab',
+'peanut butter',
+'library',
+
+'project',
+'tower of hanoi',
+'simon',
+'hangman',
+'outcomes',
+
+'homework',
+'model',
+'view',
+'resume',
+'jquery',
+
+'function',
+'variables',
+'modulus',
+'general assembly',
+'atom',
+
+'browser',
+'conlole',
+'brunch',
+'padding',
+'margin',
+'trivia',
+
+'master',
+'google',
+'scope',
+'loop',
+'dont repeat your self',
+
+'call back',
+'expressions',
+'declaration',
+'referencing',
+'parameters'
+]
